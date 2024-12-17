27ec752dd28ce813539d30a6ef736022b2e0b8a3
NicknameNeo <abdulloevaslan96@gmail.com>
2024-12-17 21:55:49 +0500

    first commit

---
+++ practic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace practic
+{
+    class Program
+    {
+        static double Nain()
+        
+        {
+            var a = Convert.ToInt32(Console.ReadLine());
+            
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Chetnoe");
+            }
+            else
+            {
+                Console.WriteLine("Nechetnoe");
+            }
+            return a;
+        }
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine(Nain());
+            Console.WriteLine(Nain());
+            Console.WriteLine(Nain());
+            Console.WriteLine(Nain());
+            Console.WriteLine(Nain());
+            
+            
+        }
+    }
+}
